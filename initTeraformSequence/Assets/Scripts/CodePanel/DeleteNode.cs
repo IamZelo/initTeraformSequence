79717fb21a17ffc0420e56f82625f8f1c68cdf34
Pratyush Tayal <pratyushtayal05@gmail.com>
2026-02-11 12:07:59 +0530

    Fix build errors

--- initTeraformSequence/Assets/Scripts/CodePanel/DeleteNode.cs
+++ initTeraformSequence/Assets/Scripts/CodePanel/DeleteNode.cs
@@ -6,7 +6,7 @@ public class DeleteNode : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag == null) return;
-
+        eventData.pointerDrag.GetComponent<BlockView>().Unlink();
         Destroy(eventData.pointerDrag);
     }
 }
